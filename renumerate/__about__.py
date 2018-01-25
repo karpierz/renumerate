91bc288248838da32f30b2d7c90851aec0e42bb1
Adam Karpierz <akarpierz@gmail.com>
2018-01-25 10:09:47 +0100

    1.0.2

--- renumerate/__about__.py
+++ renumerate/__about__.py
@@ -10,7 +10,7 @@ __title__        = "renumerate"
 __summary__      = "Reverse enumerate"
 __uri__          = "http://pypi.python.org/pypi/renumerate/"
 __version_info__ = type("version_info", (), dict(serial=0,
-                        major=1, minor=0, micro=1, releaselevel="final"))
+                        major=1, minor=0, micro=2, releaselevel="final"))
 __version__      = "{0.major}.{0.minor}.{0.micro}{1}{2}".format(__version_info__,
                    dict(final="", alpha="a", beta="b", rc="rc")[__version_info__.releaselevel],
                    "" if __version_info__.releaselevel == "final" else __version_info__.serial)
