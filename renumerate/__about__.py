edbfb68926eda5b22934a392b69932337add5096
Adam Karpierz <akarpierz@gmail.com>
2018-02-26 18:37:43 +0100

    1.0.5

--- renumerate/__about__.py
+++ renumerate/__about__.py
@@ -3,18 +3,19 @@
 # http://opensource.org/licenses/zlib
 
 __all__ = ('__title__', '__summary__', '__uri__', '__version_info__',
-           '__version__', '__author__', '__email__', '__copyright__',
-           '__license__')
+           '__version__', '__author__', '__maintainer__', '__email__',
+           '__copyright__', '__license__')
 
 __title__        = "renumerate"
 __summary__      = "Reverse enumerate"
 __uri__          = "http://pypi.python.org/pypi/renumerate/"
 __version_info__ = type("version_info", (), dict(serial=0,
-                        major=1, minor=0, micro=4, releaselevel="final"))
+                        major=1, minor=0, micro=5, releaselevel="final"))
 __version__      = "{0.major}.{0.minor}.{0.micro}{1}{2}".format(__version_info__,
                    dict(final="", alpha="a", beta="b", rc="rc")[__version_info__.releaselevel],
                    "" if __version_info__.releaselevel == "final" else __version_info__.serial)
 __author__       = "Adam Karpierz"
+__maintainer__   = "Adam Karpierz"
 __email__        = "python@python.pl"
 __copyright__    = "Copyright (c) 2016-2018 {0}".format(__author__)
 __license__      = "zlib/libpng License ; {0}".format(
